baca7d80a2e0c2bccefd573799878f8495d7cd93
Matt Gauger <mathias@heroku.com>
2016-03-28 16:43:10 -0500

    Use postgres in dev

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20150729182244) do
 
-  create_table "moods", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "moods", force: :cascade do |t|
     t.integer  "top"
     t.integer  "left"
     t.integer  "user_id"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150729182244) do
     t.string   "status"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "first_name"
     t.string   "last_name"
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 20150729182244) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
